320276f25cc78d354aa06a2b16e8171cef7487c2
pixus0120 <ra.arcus@gmail.com>
2021-07-08 12:57:12 -0400

    Create a Category List

--- Landmark/Views/Categories/CategoryHome.swift
+++ Landmark/Views/Categories/CategoryHome.swift
@@ -2,21 +2,15 @@
 //  CategoryHome.swift
 //  Landmark
 //
-//  Created by locussigilli on 6/13/21.
+//  Created by locussigilli on 7/8/21.
 //
 
 import SwiftUI
 
 struct CategoryHome: View {
-    @EnvironmentObject var modelData: ModelData
-    
-    
     var body: some View {
-        NavigationView{
-            List {
-                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in Text(key)
-                }
-            }
+        NavigationView {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .navigationTitle("Featured")
         }
     }
@@ -25,6 +19,5 @@ struct CategoryHome: View {
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
-            .environmentObject(ModelData())
     }
 }
